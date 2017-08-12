fe727ce17909a6b1a6baefc9b197be20285e7454
Dylan Marcus <dylan@mach1studios.com>
2017-08-11 22:20:15 -0400

    added volume sample collection

--- utils/list_all_files.py
+++ utils/list_all_files.py
@@ -8,3 +8,20 @@ def list_all_files(directory, extensions=None):
             joined = os.path.join(root, filename)
             if extensions is None or ext.lower() in extensions:
                 yield joined
+
+def list_all_dirs(directory):
+    return [x[0] for x in os.walk(directory)]
+
+def list_all_files_within_subdirs(directory, extensions=None):
+    alldirs = list_all_dirs(data_root)
+    allfiles = list()
+    for i in alldirs:
+        addition = list(list_all_files(i, ".wav"))
+        allfiles = allfiles + addition
+    return allfiles
+
+#data_root = "/Users/zebra/Developer/Python/AudioNotebooks/data/drums/samples/!llmind/!llmind BLAP-KIT 808s & STRING BREAKS/808s"
+data_root = "/Users/zebra/Developer/Python/AudioNotebooks/data/drums/samples/"
+print list_all_files_within_subdirs(data_root)
+#x = list(list_all_files(data_root, ".wav"))
+#print (x)
